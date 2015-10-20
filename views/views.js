2aa587ff91d2db85fa8096512eabfc5d8e4fef40
JesseZ1983 <jessezanavich@gmail.com>
2015-10-20 15:47:40 -0700

    initial upload sighting form

--- views/views.js
+++ views/views.js
@@ -2,11 +2,11 @@ var HomePageView = Backbone.View.extend({
   tagName: 'div',
   className: 'home',
   render: function(){
-    
+
     var $title       = $('<h1>').html('Lost a Pet?');
     var $foundButton = $('<button id="found-button">').html( 'Found a Pet' );
     var $lostButton  = $('<button id="lost-button">').html( 'Lost a Pet' );
-    
+
     this.$el.append( [$title, $foundButton, $lostButton] );
     this.$el.appendTo( '#master' );
   },
@@ -29,18 +29,69 @@ var HomePageView = Backbone.View.extend({
 });
 
 var UploadSightingView = Backbone.View.extend({
-  tagName: 'section',
+  tagName: 'div',
   className: 'upload',
-  //template: Handlebars.compile( $('#hbs1').html() ),
   render: function(){
-    console.log( 'found a pet button works!' );
+
+    var $uploadForm = $('<form id="upload-form">');
+    var $uploadPhoto = $('<input id="upload-photo" type="file"> </input>');
+
+    $uploadForm.append($uploadPhoto);
+    $(this.$el).append($uploadForm);
+    $('#master').append(this.$el);
+
   },
   initialize: function( options ){
     _.extend( options );
     this.render();
   },
   events: {
+    'change #upload-photo' : 'uploadPhoto'
+  },
+  uploadPhoto: function() {
+  //Sighting Form
+    //Location
+    var $uploadLocationLabel = $('<label for="uploadLocation">').html("<p> Location: </p>");
+    var $uploadLocation = $('<input id="uploadLocation" type="text" name="location">');
+    //Date
+    var $uploadDateLabel = $('<label for="uploadDate">').html('<p> Date: </p>');
+    var $uploadDate = $('<input id="uploadDate" type="text" name="date">');
+    //Species
+    var $uploadSpeciesLabel = $('<label for="uploadSpecies">').html('<p> Species </p>');
+    var $uploadSpecies = $('<select id="uploadSpecies name="species">').html(
+      '<option value=""> </option> <option value="dog"> Dog </option>   <option value="cat"> Cat </option>'
+    );
+    //Size
+    var $uploadSize = $('<fieldset>');
+    var $uploadSizeLegend = $('<legend> Please select size: </legend>');
 
+    var $uploadSmall = $('<input id="uploadSmall" type="radio" name="size" value="small">');
+    var $uploadSmallLabel = $('<label for="uploadSmall">').html('Small');
+
+    var $uploadMedium = $('<input id="uploadMedium" type="radio" name="size" value="medium">');
+    var $uploadMediumLabel = $('<label for="uploadMedium">').html('Medium');
+
+    var $uploadLarge = $('<input id="uploadMedium" type="radio" name="size" value="Large">');
+    var $uploadLargeLabel = $('<label for="uploadLarge">').html('Large');
+
+    $uploadSizeLegend.append($uploadSmall, $uploadSmallLabel, $uploadMedium, $uploadMediumLabel, $uploadLarge, $uploadLargeLabel);
+    $uploadSize.html($uploadSizeLegend);
+    //Color
+    var $uploadColor = $('<fieldset>');
+    var $uploadColorLegend = $('<legend> Please select color: </legend>');
+
+    var $uploadBrown = $('<input id="uploadBrown" type="checkbox" name="color">');
+    var $uploadBrownLabel = $('<label for="uploadBrown">').html('Brown');
+
+    $uploadColorLegend.append($uploadBrown, $uploadBrownLabel);
+    $uploadColor.html($uploadColorLegend);
+    //Description
+    var $uploadDescription = $('<textarea id="uploadDescription" name="description">');
+    var $uploadDescriptionLabel = $('<label for="uploadDescription">').html('Enter description:');
+    //Submit and Append Form
+    var $uploadSubmit = $('<input type="submit" value="submit">');
+
+    $('#upload-form').append($uploadLocationLabel, $uploadLocation, $uploadDateLabel, $uploadDate, $uploadSpeciesLabel, $uploadSpecies, $uploadSize, $uploadColor, $uploadDescriptionLabel, $uploadDescription, $uploadSubmit );
   }
 });
 
@@ -75,11 +126,11 @@ var SearchFormView = Backbone.View.extend({
 
     this.remove();
     // var coll = app.collection.fetch({data: {filter : searchValues}});
-    var searchResultsPage = new ListView({ 
+    var searchResultsPage = new ListView({
       collection : new lostPetsCollection({data:searchValues}),
       searchValues : searchValues
     });
-    
+
   }
 });
 
@@ -104,7 +155,7 @@ var ListView = Backbone.View.extend({
   initialize: function( options ) {
     _.extend( this, options );
     this.render();
-  }, 
+  },
   events: {
     "click #edit" : "editSearch",
     "click #map" : "mapView"
@@ -156,6 +207,6 @@ var MapView = Backbone.View.extend({
 // date = string
 // address = string
 // radius = string
-// animal-type = string 
+// animal-type = string
 // color-group = array of strings
 // size-group = string
