4c53f57c1d2a4c383b08156a03a429faa474b905
Joe Fraley <jose.fraley@gmail.com>
2015-10-22 11:38:15 -0700

    includes cloudinary in the server. has a stupid fake route for demonstration

--- server.js
+++ server.js
@@ -13,6 +13,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false}));
 app.use(express.static(__dirname));
 
+//==================================
+// MULTIPART FORM ENCODER CODE HERE
+var multer = require('multer');
+var upload = multer({dest:'./uploads/'})
+// var upload = multer({});
+//==================================
+
+//==================================
+//   CLOUD STORAGE CODE HERE
+var cloudinary = require('cloudinary');
+var uploader   = cloudinary.uploader;
+
+cloudinary.config({
+  cloud_name: "dxiaaofss",
+     api_key: "779484679453537",
+  api_secret: "WR-ESHlfepbHtul_9S5MR7iEVCs"
+});
+//==================================
+
+
+
+
 app.get('/pet', function(request, response) {
 	var query = "value.animalType: (" + request.query.animalType + ") AND value.size: (" + request.query.size + ")";
 	db.search('sighting', query) // params?  data?  body?
@@ -35,6 +57,14 @@ app.post('/pet', function(request, response) {
 		    console.log(err);
 	});
 
+});
+
+app.post('/photoUpload', upload.single('image'), function (req,res){
+  console.log('req.file =', req.file);
+  
+  uploader.upload( req.file.path, function (result)  {
+    console.log('return after upload: ', result);
+  });
 });
 
 app.listen(3000);
