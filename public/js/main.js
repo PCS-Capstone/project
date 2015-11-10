d4142c26c74eb0309bf49bf144c4ea9d6ab43b0e
JesseZ1983 <jessezanavich@gmail.com>
2015-11-09 16:41:15 -0800

    Sighting: CSS Styling to upload photo

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,8 @@ var router = new Router();
 app.searchParameters = {};
 var currentView;
 
+var counter = 0;
+
 $(document).ready( function(){
   Backbone.history.start({pushState: true});
   router.navigate('', {trigger: true});
@@ -12,6 +14,15 @@ $(document).ready( function(){
 });
 
 window.addEventListener('popstate', function(e) {
-	currentView.remove();
+  if (counter != 0) {
+    counter += 1;
+    currentView.remove();
     router.navigate(Backbone.history.getFragment(), {trigger: true, replace: true});
+  }
+  else {
+    counter += 1;
+    router.navigate(Backbone.history.getFragment(), {trigger: true, replace: true});
+  }
 });
+
+counter += 1;
